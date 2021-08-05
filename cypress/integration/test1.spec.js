d21385c49a4e23b2f0350acbd368990a552acc88
Justin Lin <34898169+ghostcat17@users.noreply.github.com>
2021-08-04 17:15:06 -0700

    update test1

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -2,6 +2,6 @@ describe('My Second Test', () => {
     it('finds the content "title"', () => {
       cy.visit('https://www.google.com')
   
-      cy.contains("Google")
+       cy.title().should('contain', "Google")
     })
   })
