40280799c69ba54e797b24e814e2ea5c92f9f40e
Justin Lin <34898169+ghostcat17@users.noreply.github.com>
2021-08-04 17:11:14 -0700

    test1 update

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -1,7 +1,7 @@
 describe('My Second Test', () => {
     it('finds the content "title"', () => {
-      cy.visit('https://www.reuters.com')
+      cy.visit('https://www.google.com')
   
-      cy.title().should('contain', "Home | Reuters")
+      cy.contains("Google")
     })
   })
